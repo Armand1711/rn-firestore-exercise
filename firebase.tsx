8fb0aec4f81535bfa3e9e70250b92a8bbef0a61a
Armand1711 <125480832+Armand1711@users.noreply.github.com>
2025-07-30 12:35:42 +0200

    Initialize Cloud Firestore with Firebase configuration and export db reference

--- firebase.tsx
+++ firebase.tsx
@@ -1,3 +1,19 @@
-//TODO: Initialise Cloud Firestore and get a reference to the service
-//Documentation: https://docs.expo.dev/guides/using-firebase/
+// TODO: Initialise Cloud Firestore and get a reference to the service
+// Documentation: https://docs.expo.dev/guides/using-firebase/
 
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCcyHpVVC80J4iPGu5Yyjp-aUWKu93tVPQ",
+  authDomain: "dv300-class-exercise.firebaseapp.com",
+  projectId: "dv300-class-exercise",
+  storageBucket: "dv300-class-exercise.appspot.com", 
+  messagingSenderId: "936643027240", 
+  appId: "1:936643027240:web:12f0131ac6cfa0ebd0b01d"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export { db };
